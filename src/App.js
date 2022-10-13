d3e4fbec681adc9f37afe021cb1b2f5acdb7e618
Adeagbo Oluwatobi <adeagbotobi@gmail.com>
2022-10-13 21:24:48 +0100

    components/product-card/product-card.component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component'
+import ProductCard from  './components/product-card/product-card.component'
 
 const App = () => {
     return (
